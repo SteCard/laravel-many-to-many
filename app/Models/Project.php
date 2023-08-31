d386ff994fd07547f2f1efa2650efd0a52aed2a2
SteCard <stefanocardillo14@gmail.com>
2023-08-31 02:01:01 +0200

    models project+technology relations

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 // use\App\Models\Type;
+use App\Models\Technology;
 
 class Project extends Model
 {
@@ -15,6 +16,10 @@ class Project extends Model
 
     public function type(){
         return $this->belongsTo(Type::class);
+    }
+
+    public function technologies(){
+        return $this->belongsToMany(Technology::class);
     }
 
 }
